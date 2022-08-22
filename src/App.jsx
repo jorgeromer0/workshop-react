5c03ab5d717a36660b66649553691b61b2613005
Jorge <jorgerom@hotmail.es>
2022-08-22 19:37:46 +0200

    Dia 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
-import { useState, useEffect } from "react";
-import { Heading, Box, Image, Text, Flex, Spacer, Tag, Icon } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+
+import { LaunchList } from "./components/LaunchList";
+import { LaunchDetails } from "./components/LaunchDetails";
+import { RocketDetails } from "./components/RocketDetails";
 import logo from "./assets/logo-spacex.png";
-import * as API from "./services/launches";
-import { LaunchItem } from "./components/LaunchItem";
 
-function App() {
-  const [launches, setLaunches] = useState([]);
-
-  useEffect(() => {
-    API.getAllLaunches().then(setLaunches);
-  }, []);
-
+export function App() {
   return (
     <>
-      <Image src={logo} width="300" m={4} />
-      <Heading as="h1" size="lg" m={4}>
-        SpaceX Launches
-      </Heading>
-      <section>
-      {launches.map((launch) => (
-          <LaunchItem key={launch.flight_number} {...launch} />
-        ))}
-      </section>
+      <Image m={4} src={logo} width={300} />
+      <Routes>
+        <Route path="/" element={<LaunchList />} />
+        <Route path="launch/:launchId" element={<LaunchDetails />} />
+        <Route path="rockets/:rocketId" element={<RocketDetails />} />
+      </Routes>
     </>
   );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { ChakraProvider } from '@chakra-ui/react'
 
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { App } from "./App";
+
+ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-    <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
-  
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+  document.getElementById("root")
+);
